684c4d4b175d4612f25d2db1b38ed5cc5c5c1d59
denis281542 <deniskuimov@mail.ru>
2019-10-18 16:36:16 +0500

    Create landing index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on('turbolinks:load', function(){
+  console.log('yiha, we are ready!')
+})
