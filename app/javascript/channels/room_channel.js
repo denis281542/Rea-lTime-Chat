ed45355d8e8cc1bb6571c15531149b7b4e8ad06b
denis281542 <deniskuimov@mail.ru>
2019-10-19 12:36:32 +0500

    Edit room_channel message->msg

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -12,7 +12,7 @@ consumer.subscriptions.create("RoomChannel", {
   
   received(data) {
 
-    $('#message_holder').append('<div class="message">' + data.content + '</div>')
+    $('#msg').append('<div class="message">' + data.content + '</div>')
     console.log(data)
     // Called when there's incoming data on the websocket for this channel
   }
