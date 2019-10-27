407f8c13893bb6c0d3f71b0aeb9a70e5a7e9d73a
denis281542 <deniskuimov@mail.ru>
2019-10-27 23:11:52 +0500

    Edit method search in posts model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.search(params[:search])
+    @posts = Post.where("post LIKE ?", "%#{search}%")
   end
 
   # GET /posts/1
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
 
   def self.search(search)
     if search
-      where("name LIKE ?", "%#{search}%")
+      where("post LIKE ?", "%#{params[:search]}%")
     else
       all
     end
