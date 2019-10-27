6c2f42dc8d8313668002453b8d63f36a828a8ea2
denis281542 <deniskuimov@mail.ru>
2019-10-27 22:57:18 +0500

    Added method search in posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ApplicationRecord
 
   has_many      :comments, dependent: :destroy
 
+  def self.search(search)
+    if search
+      where("name LIKE ?", "%#{search}%")
+    else
+      all
+    end
+  end
+
 end
